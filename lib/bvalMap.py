b7630561945c966580cd1639310d366a7f356b18
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-02-25 11:35:30 -0500

    bmap condition, bval append

--- lib/bvalMap.py
+++ lib/bvalMap.py
@@ -5,37 +5,39 @@ with warnings.catch_warnings():
 
     from dipy.io.image import load_nifti, save_nifti
     from dipy.io import read_bvals_bvecs
-    from dipy.reconst.shm import normalize_data
+    from normalize import normalize_data
+    #from dipy.reconst.shm import normalize_data
     from dipy.segment.mask import applymask
     from scipy.io import savemat
 
 import numpy as np
 
-def bvalMap(dwi, bvals, bmax):
+def remapBval(dwi, bvals, bmax):
 
     # find b0
     where_b0= np.where(bvals == 0)[0]
-    b0= dwi[...,where_b0].mean(-1)
 
     # normalize dwi by b0
-    dwiPrime= normalize_data(dwi, where_b0)
+    dwiPrime, b0= normalize_data(dwi, where_b0)
 
     # scale signal to the power of bmax/b
     ratio= []
+    bvalsNew= []
     for b in bvals:
-        if b:
+        if b and (b>1.01*bmax or b<0.99*bmax):
             ratio.append(bmax/b)
+            bvalsNew.append(bmax)
         else:
             ratio.append(1.)
+            bvalsNew.append(0)
 
     ratio= np.reshape(ratio, (1, len(bvals)))
     dwiHat= dwiPrime**ratio
 
     # un-normalize dwi by b0
     dwiNew= applymask(dwiHat, b0)
-    bvalsNew= bvals*bmax/max(bvals)
 
-    return (dwiNew, bvalsNew)
+    return (dwiNew, np.array(bvalsNew))
 
 
 if __name__=='__main__':
@@ -43,14 +45,13 @@ if __name__=='__main__':
     mask= load_nifti('/home/tb571/Downloads/Harmonization-Python/connectom_prisma_demoData/A/connectom/mask.nii.gz')[0]
     bvals, _= read_bvals_bvecs(
         '/home/tb571/Downloads/Harmonization-Python/connectom_prisma_demoData/A/connectom/dwi_A_connectom_st_b1200.bval',
-        '/home/tb571/Downloads/Harmonization-Python/connectom_prisma_demoData/A/connectom/dwi_A_connectom_st_b1200.bvec')
-    dwi= bvalMap(dwi, bvals, 300)
-    savemat('/home/tb571/Downloads/Harmonization-Python/connectom_prisma_demoData/bmap/b_data.mat',{'dwi_mat':dwi})
-    save_nifti('/home/tb571/Downloads/Harmonization-Python/connectom_prisma_demoData/bmap/dwi.nii.gz', dwi, affine)
+        None)
+    dwi, bvalsNew= remapBval(dwi, bvals, 300)
+    # save_nifti('/home/tb571/Downloads/Harmonization-Python/connectom_prisma_demoData/bmap/dwi.nii.gz', dwi, affine)
 
     '''
     # for testing in MATLAB
-        
+    # savemat('/home/tb571/Downloads/Harmonization-Python/connectom_prisma_demoData/bmap/b_data.mat',{'dwi_mat':dwi})
     dwPrime= np.random.rand(3*3*5*5).reshape((3,3,5,5))
     bvals= np.reshape([50, 100, 200, 50, 200], (5,1))
     bmax= 300
