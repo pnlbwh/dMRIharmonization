bbdf454de388307af7e4962993448c9e118f6c4e
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-03-25 10:14:16 -0400

    concise code

--- lib/normalize.py
+++ lib/normalize.py
@@ -11,10 +11,8 @@ def normalize_data(dwi, where_b0= None, b0= None):
     if b0 is None:
         b0= find_b0(dwi, where_b0)
 
+    np.nan_to_num(b0).clip(min=1., out=b0)
     dwiPrime= dwi/b0[...,None]
     np.nan_to_num(dwiPrime).clip(min=0., max= 1., out= dwiPrime)
-    # dwiPrime[np.isnan(dwiPrime)]= 0.
-    # dwiPrime[dwiPrime<0]= 0.
-    # dwiPrime[dwiPrime>1]= 1.
 
     return (dwiPrime, b0)
