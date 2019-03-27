a8311afa0377468f4cb0955c0c3fef86ef71b648
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-03-27 14:20:20 -0400

    major revision

--- lib/debug_fa.py
+++ lib/debug_fa.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-import warnings, os, configparser, multiprocessing, shutil
+import warnings, os, configparser, multiprocessing
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=FutureWarning)
     import nibabel as nib
-    from dipy.segment.mask import applymask
 
 from plumbum.cmd import antsApplyTransforms
 from plumbum import FG
@@ -13,141 +12,116 @@ from cleanOutliers import antsReg
 
 SCRIPTDIR= os.path.dirname(__file__)
 ROOTDIR= os.path.abspath(os.path.join(SCRIPTDIR, '..'))
+mniTmp = os.path.join(ROOTDIR, 'IITAtlas', 'IITmean_FA.nii.gz')
+
 config = configparser.ConfigParser()
 config.read(os.path.join(SCRIPTDIR,'config.ini'))
 N_proc = int(config['DEFAULT']['N_proc'])
-diffusionMeasures= [x for x in config['DEFAULT']['diffusionMeasures'].split(',')]
+diffusionMeasures = [x for x in config['DEFAULT']['diffusionMeasures'].split(',')]
 
-def reg2standard(imgPath, warp2mni, trans2mni, ref, tar_harm, tar_unharm, fixed, templatePath, siteName):
+def register_reference(imgPath, mniTmp, warp2mni, trans2mni):
 
     print(f'Warping {imgPath} diffusion measures to standard space')
     directory = os.path.dirname(imgPath)
     inPrefix = imgPath.split('.')[0]
     prefix = os.path.split(inPrefix)[-1]
 
-    # reference site have been already warped to template space in buildTemplate.py: warp_bands()
-    # warped data are os.path.join(directory, 'dti', prefix + f'_WarpedFA.nii.gz')
-
-    # template FA was warped to target sites in cleanOutliers.py: antsApply()
-    # to go from target site to template space, use the following files:
-    trans2sub = os.path.join(directory, 'harm', f'ToSubjectSpace_{prefix}0GenericAffine.mat')
-    warp2tmp = os.path.join(directory, 'harm', f'ToSubjectSpace_{prefix}1InverseWarp.nii.gz')
-
-    # only harmonized data needs to SyN register to template space, identified with 'harmonized_' in prefix
-    if 'harmonized_' in prefix:
-        tar_unharm = True
-
-        # # uncomment the following lines to register harmonized data to target site
-        # dmImg = os.path.join(directory, 'dti', prefix + f'_FA.nii.gz')
-        # dmTmp = os.path.join(templatePath, f'Mean_{siteName}_FA.nii.gz')
-        # maskTmp = os.path.join(templatePath, f'{siteName}_Mask.nii.gz')
-        # outPrefix = os.path.join(directory, 'dti', prefix + '_FA')
-        # antsReg(dmTmp, maskTmp, dmImg, outPrefix)
-        # warp2tmp = outPrefix + '1Warp.nii.gz'
-        # trans2tmp = outPrefix + '0GenericAffine.mat'
-        # tar_harm = True
-
-    elif os.path.exists(trans2sub) and os.path.exists(warp2tmp):
-        tar_unharm = True
-
-        # uncomment the following to use warp/affine created during
-        # registering template FA to each subject space in the target site
-        # this way we don't have to register harmonized data to template space again
-        shutil.copyfile(trans2sub,
-                        os.path.join(directory, 'harm', f'ToSubjectSpace_harmonized_{prefix}0GenericAffine.mat'))
-        shutil.copyfile(warp2tmp,
-                        os.path.join(directory, 'harm', f'ToSubjectSpace_harmonized_{prefix}1InverseWarp.nii.gz'))
-
-
-    else:
-        ref = True
-
     for dm in diffusionMeasures:
 
         output = os.path.join(directory, 'dti', prefix + f'_InMNI_{dm}.nii.gz')
 
-        if ref:
-            moving = os.path.join(directory, 'dti', prefix + f'_Warped{dm}.nii.gz')
-            # warp diffusion measure to MNI space directly
-            antsApplyTransforms[
-                '-d', '3',
-                '-i', moving,
-                '-o', output,
-                '-r', fixed,
-                '-t', warp2mni, trans2mni
-            ] & FG
+        # reference site have been already warped to reference template space in buildTemplate.py: warp_bands()
+        # warped data are os.path.join(directory, 'dti', prefix + f'_WarpedFA.nii.gz')
+        moving = os.path.join(directory, 'dti', prefix + f'_Warped{dm}.nii.gz')
 
-        elif tar_unharm:
-            moving = os.path.join(directory, 'dti', prefix + f'_{dm}.nii.gz')
-            # warp diffusion measure to template space first, then to MNI space
-            antsApplyTransforms[
-                '-d', '3',
-                '-i', moving,
-                '-o', output,
-                '-r', fixed,
-                '-t', warp2mni, trans2mni, f'[{trans2sub},1]', warp2tmp
-            ] & FG
+        # so warp diffusion measure to MNI space directly
+        antsApplyTransforms[
+            '-d', '3',
+            '-i', moving,
+            '-o', output,
+            '-r', mniTmp,
+            '-t', warp2mni, trans2mni
+        ] & FG
 
-            # dmImg = os.path.join(directory, 'dti', prefix + f'_FA.nii.gz')
-            # dmTmp = os.path.join(templatePath, f'Mean_{siteName}_FA.nii.gz')
-            # maskTmp = os.path.join(templatePath, f'{siteName}_Mask.nii.gz')
-            # outPrefix = os.path.join(directory, 'dti', prefix + '_FA')
-            # antsReg(dmTmp, maskTmp, dmImg, outPrefix)
-            # warp2tmp = outPrefix + '1Warp.nii.gz'
-            # trans2tmp = outPrefix + '0GenericAffine.mat'
-            #
-            # # warp diffusion measure to template space first
-            # antsApplyTransforms[
-            #     '-d', '3',
-            #     '-i', dmImg,
-            #     '-o', os.path.join(directory, 'dti', prefix + f'_Warped{dm}.nii.gz'),
-            #     '-r', dmTmp,
-            #     '-t', warp2tmp, trans2tmp
-            # ] & FG
-            #
-            # moving = os.path.join(directory, 'dti', prefix + f'_Warped{dm}.nii.gz')
-            # antsApplyTransforms[
-            #     '-d', '3',
-            #     '-i', moving,
-            #     '-o', output,
-            #     '-r', fixed,
-            #     '-t', warp2mni, trans2mni
-            # ] & FG
 
-        elif tar_harm:
-            moving = os.path.join(directory, 'dti', prefix + f'_{dm}.nii.gz')
-            # warp diffusion measure to template space first, then to MNI space
-            antsApplyTransforms[
-                '-d', '3',
-                '-i', moving,
-                '-o', output,
-                '-r', fixed,
-                '-t', warp2mni, trans2mni, warp2tmp, trans2tmp
-            ] & FG
+def register_target(imgPath, mniTmp):
+    print(f'Warping {imgPath} diffusion measures to standard space')
+    directory = os.path.dirname(imgPath)
+    inPrefix = imgPath.split('.')[0]
+    prefix = os.path.split(inPrefix)[-1]
 
-def sub2tmp2mni(templatePath, siteName, caselist):
+    dmImg = os.path.join(directory, 'dti', prefix + f'_FA.nii.gz')
+
+    outPrefix = os.path.join(directory, 'dti', prefix + '_FA_ToMNI_')
+    antsReg(mniTmp, None, dmImg, outPrefix)
+    warp2mni = outPrefix + '1Warp.nii.gz'
+    trans2mni = outPrefix + '0GenericAffine.mat'
+
+    for dm in diffusionMeasures:
+        output = os.path.join(directory, 'dti', prefix + f'_InMNI_{dm}.nii.gz')
+
+        moving = os.path.join(directory, 'dti', prefix + f'_{dm}.nii.gz')
+        # warp diffusion measure to template space first, then to MNI space
+        antsApplyTransforms[
+            '-d', '3',
+            '-i', moving,
+            '-o', output,
+            '-r', mniTmp,
+            '-t', warp2mni, trans2mni,
+        ] & FG
+
+
+def register_harmonized(imgPath, mniTmp, warp2mni, trans2mni, templatePath, siteName):
+
+    print(f'Warping {imgPath} diffusion measures to standard space')
+    directory = os.path.dirname(imgPath)
+    inPrefix = imgPath.split('.')[0]
+    prefix = os.path.split(inPrefix)[-1]
+
+    dmImg = os.path.join(directory, 'dti', prefix + f'_FA.nii.gz')
+    dmTmp = os.path.join(templatePath, f'Mean_{siteName}_FA.nii.gz')
+    maskTmp = os.path.join(templatePath, f'{siteName}_Mask.nii.gz')
+    outPrefix = os.path.join(directory, 'dti', prefix + '_FA')
+    antsReg(dmTmp, maskTmp, dmImg, outPrefix)
+    warp2tmp = outPrefix + '1Warp.nii.gz'
+    trans2tmp = outPrefix + '0GenericAffine.mat'
+
+    for dm in diffusionMeasures:
+        output = os.path.join(directory, 'dti', prefix + f'_InMNI_{dm}.nii.gz')
+
+        moving = os.path.join(directory, 'dti', prefix + f'_{dm}.nii.gz')
+        # warp diffusion measure to template space first, then to MNI space
+        antsApplyTransforms[
+            '-d', '3',
+            '-i', moving,
+            '-o', output,
+            '-r', mniTmp,
+            '-t', warp2mni, trans2mni, warp2tmp, trans2tmp
+        ] & FG
+
+
+def sub2tmp2mni(templatePath, siteName, caselist, ref= False, tar_unproc= False, tar_harm= False):
 
     # obtain the transform
     moving = os.path.join(templatePath, f'Mean_{siteName}_FA.nii.gz')
-    fixed = os.path.join(ROOTDIR, 'IITAtlas', 'IITmean_FA.nii.gz')
 
     outPrefix= os.path.join(templatePath, f'TemplateToMNI_{siteName}')
     warp2mni= outPrefix+'1Warp.nii.gz'
     trans2mni= outPrefix+'0GenericAffine.mat'
     if not os.path.exists(warp2mni):
-        antsReg(fixed, None, moving, outPrefix)
+        antsReg(mniTmp, None, moving, outPrefix)
 
     imgs, _= read_caselist(caselist)
 
-
-    ref = False
-    tar_harm = False
-    tar_unharm = False
-
     pool= multiprocessing.Pool(N_proc)
     for imgPath in imgs:
-        pool.apply_async(func= reg2standard, args= (imgPath, warp2mni, trans2mni, ref, tar_harm, tar_unharm,
-                                                         fixed, templatePath, siteName, ))
+
+        if ref:
+            pool.apply_async(func= register_reference, args= (imgPath, warp2mni, trans2mni, ))
+        elif tar_unproc:
+            pool.apply_async(func= register_target, args= (imgPath, mniTmp, ))
+        if tar_harm:
+            pool.apply_async(func= register_harmonized, args= (imgPath, warp2mni, trans2mni, mniTmp, templatePath, siteName, ))
 
     pool.close()
     pool.join()
