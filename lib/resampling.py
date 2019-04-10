fa8438789918c85f18dccbaccba7f2a2ad1cd138
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-04-10 12:31:55 -0400

    major revision

--- lib/resampling.py
+++ lib/resampling.py
@@ -13,7 +13,6 @@ from skimage.transform import resize
 from scipy.ndimage import binary_opening, generate_binary_structure
 from subprocess import check_call
 from normalize import normalize_data, find_b0
-eps= 2.2204e-16
 
 
 def save_high_res(fileName, sp_high, lowResImgHdr, highResImg):
@@ -25,7 +24,8 @@ def save_high_res(fileName, sp_high, lowResImgHdr, highResImg):
     scale= np.diag((sp_high/sp_low).tolist()+[1.])
     imgHdrOut.set_sform(imgHdrOut.get_sform() @ scale)
     imgHdrOut.set_qform(imgHdrOut.get_qform() @ scale)
-    save_nifti(fileName, highResImg, affine= imgHdrOut.get_qform(), hdr=imgHdrOut)
+    save_nifti(fileName, highResImg, affine= imgHdrOut.get_sform(), hdr=imgHdrOut)
+
 
 def resampling(lowResImgPath, lowResMaskPath, lowResImg, lowResImgHdr, lowResMask, lowResMaskHdr, sp_high, bvals):
 
@@ -33,9 +33,9 @@ def resampling(lowResImgPath, lowResMaskPath, lowResImg, lowResImgHdr, lowResMas
     sOrder= 5
 
     # resample the dwi ----------------------------------------------------------------
-    lowResImg= applymask(lowResImg, lowResMask)
-    where_b0= np.where(bvals == 0)[0]
-    b0= find_b0(lowResImg, where_b0= where_b0)
+    where_b0 = np.where(bvals == 0)[0]
+    lowResImgPrime, b0= normalize_data(lowResImg, mask= lowResMask, where_b0= where_b0)
+    lowResImg= applymask(lowResImgPrime, b0)
 
     sp_low= lowResImgHdr['pixdim'][1:4]
     step = sp_low/sp_high
@@ -44,31 +44,33 @@ def resampling(lowResImgPath, lowResMaskPath, lowResImg, lowResImgHdr, lowResMas
     highResImg= np.zeros((sx, sy, sz, lowResImg.shape[3]), dtype='float')
     for i in np.where(bvals != 0)[0]:
         print('Resampling gradient ', i)
-        highResImg[:,:,:,i]= resize(np.double(lowResImg[:,:,:,i]), (sx, sy, sz), order= sOrder, mode= 'edge')
+        highResImg[:,:,:,i]= resize(lowResImg[:,:,:,i], (sx, sy, sz), order= sOrder, mode= 'constant')
 
     # resample the mask ---------------------------------------------------------------
-    highResMaskPath = lowResMaskPath.split('.')[0] + '_resampled' + '.nii.gz'
-    highResMask= resize(np.double(lowResMask), (sx, sy, sz), order= 1, mode= 'edge') # order 1 for linear interpolation
+    highResMaskPath = lowResMaskPath.split('.')[0] + '_resampled.nii.gz'
+    highResMask= resize(lowResMask.astype('float'), (sx, sy, sz), order= 1, mode= 'constant') # order 1 for linear interpolation
     highResMask= binary_opening(highResMask >= 0.5, structure=generate_binary_structure(3, 1)) * 1
-    save_high_res(highResMaskPath, sp_high, lowResMaskHdr, highResMask.astype(int))
+    save_high_res(highResMaskPath, sp_high, lowResMaskHdr, highResMask.astype('int16'))
 
 
     # resample the b0 ----------------------------------------------------------------
-    highResB0Path= lowResImgPath.split('.')[0] + '_resampled_bse' + '.nii.gz'
-    b0HighRes= resize(np.double(b0), (sx, sy, sz), order= sOrder, mode= 'edge')
-    np.nan_to_num(b0HighRes).clip(min= 1., out= b0HighRes)
-    save_high_res(highResB0Path, sp_high, lowResMaskHdr, b0HighRes)
+    highResB0PathTmp= lowResImgPath.split('.')[0] + '_resampled_bse_tmp.nii.gz'
+    b0HighRes= resize(b0, (sx, sy, sz), order= sOrder, mode= 'constant')
+    np.nan_to_num(b0HighRes).clip(min= 0., out= b0HighRes) # using min= 1. is unnecessary
+    save_high_res(highResB0PathTmp, sp_high, lowResMaskHdr, b0HighRes)
 
     # unring the b0
-    check_call(['unring.a64', highResB0Path, highResB0Path])
+    highResB0Path = lowResImgPath.split('.')[0] + '_resampled_bse.nii.gz'
+    check_call(['unring.a64', highResB0PathTmp, highResB0Path])
+    check_call(['rm', highResB0PathTmp])
     b0_gibs = nib.load(highResB0Path).get_data()
+    np.nan_to_num(b0_gibs).clip(min= 0., out= b0_gibs) # using min= 1. is unnecessary
 
     # defining lh_max and lh_min separately to deal with memory error
     lh_max= b0.max()
     lh_min= b0.min()
     b0_gibs[b0_gibs > lh_max] = lh_max
     b0_gibs[b0_gibs < lh_min] = lh_min
-    np.nan_to_num(b0_gibs).clip(min= 1., out= b0_gibs)
     save_high_res(highResB0Path, sp_high, lowResMaskHdr, b0_gibs)
 
 
@@ -82,9 +84,7 @@ def resampling(lowResImgPath, lowResMaskPath, lowResImg, lowResImgHdr, lowResMas
     highResImg[highResImg < lh_min] = lh_min
 
     highResImg= applymask(highResImg, highResMask)
-    highResImgPath= lowResImgPath.split('.')[0]+'_resampled'+'.nii.gz'
-    highResImg, _= normalize_data(highResImg, b0= b0_gibs)
-    highResImg= applymask(highResImg, b0_gibs)
+    highResImgPath= lowResImgPath.split('.')[0]+'_resampled.nii.gz'
     save_high_res(highResImgPath, sp_high, lowResImgHdr, highResImg)
 
     return (highResImgPath, highResMaskPath)
