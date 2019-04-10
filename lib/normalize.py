c6603dfd113cfa90d080d1046038c74a3b8e2a87
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-04-10 12:26:45 -0400

    major revision

--- lib/normalize.py
+++ lib/normalize.py
@@ -1,18 +1,36 @@
-import numpy as np
+from numpy import nan_to_num
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    from dipy.segment.mask import applymask
 
-def find_b0(dwi, where_b0):
+def find_b0(dwi, where_b0, mask= None):
     b0= dwi[...,where_b0].mean(-1)
-    np.nan_to_num(b0).clip(min= 1., out= b0)
+    nan_to_num(b0).clip(min= 0., out= b0)
 
-    return b0
+    if mask is not None:
+        return b0*mask
+    else:
+        return b0
 
 
-def normalize_data(dwi, where_b0= None, b0= None):
-    if b0 is None:
-        b0= find_b0(dwi, where_b0)
+def normalize_data(dwi, where_b0= None, mask= None, b0= None):
 
-    np.nan_to_num(b0).clip(min=1., out=b0)
+    dwi= dwi.astype('float32')
+
+    if where_b0 is not None and b0 is None:
+        b0= find_b0(dwi, where_b0, mask)
+        nan_to_num(b0).clip(min=1., out=b0)
+        for i in where_b0:
+            dwi[...,i] = b0
+    else:
+        nan_to_num(b0).clip(min=1., out=b0)
+
+
     dwiPrime= dwi/b0[...,None]
-    np.nan_to_num(dwiPrime).clip(min=0., max= 1., out= dwiPrime)
+    nan_to_num(dwiPrime).clip(min=0., max= 1., out= dwiPrime)
+
+    if mask is not None:
+        dwiPrime= applymask(dwiPrime, mask)
 
     return (dwiPrime, b0)
