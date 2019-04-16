237a40049edc652891f30f97637afed13fa793ab
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-04-16 16:38:52 -0400

    full sampled data, zero padding, qb_model_b0_mask

--- lib/cleanOutliers.py
+++ lib/cleanOutliers.py
@@ -1,18 +1,8 @@
-from subprocess import check_call
-import os, configparser, shutil
 from skimage.measure import label, regionprops
-from scipy.ndimage import binary_erosion, binary_dilation, \
-    generate_binary_structure, iterate_structure
-import numpy as np
+from scipy.ndimage import binary_erosion, binary_dilation, generate_binary_structure, iterate_structure
 
-import warnings
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=FutureWarning)
-    from dipy.io.image import load_nifti, save_nifti
-    from dipy.segment.mask import applymask
-
+from util import *
 from buildTemplate import applyXform
-from rish import rish
 from local_med_filter import local_med_filter
 from preprocess import dti_harm, preprocessing
 
@@ -23,6 +13,7 @@ config.read(os.path.join(SCRIPTDIR,'config.ini'))
 N_shm = int(config['DEFAULT']['N_shm'])
 N_proc = int(config['DEFAULT']['N_proc'])
 debug = int(config['DEFAULT']['debug'])
+n_zero = int(config['DEFAULT']['N_zero'])
 
 def antsReg(img, mask, mov, outPrefix):
 
@@ -79,26 +70,25 @@ def findLargestConnectMask(img, mask):
     return mask
 
 
-def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model):
+def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model, hdr):
 
     B = qb_model.B
-    bvals= qb_model.gtab.bvals
+    affine= hdr.get_best_affine()
 
     mapped_cs= []
     shs_same_level= [[0, 1], [1, 6], [6, 15], [15, 28], [28, 45]]
 
-    mask, _ = load_nifti(maskPath)
+    mask= load(maskPath).get_data()
     for i in range(0, N_shm+1, 2):
 
         # load data and mask
         fileName= os.path.join(directory, 'harm', f'Scale_L{i}_{prefix}.nii.gz')
-        img, affine = load_nifti(fileName)
+        img= load(fileName).get_data()
 
         if i==0: # compute the skullRingMask from 0th shm
 
             mask= findLargestConnectMask(img, mask)
 
-            n_zero= 5
             se= custom_spherical_structure(n_zero)
             paddedMask= np.pad(mask, n_zero, 'constant', constant_values= 0.)
 
@@ -119,7 +109,7 @@ def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model):
             mask_final= findLargestConnectMask(denoisedImg, mask)
             # mask_final= binary_dilation(mask_final, generate_binary_structure(3,1))*1
             harmMask = os.path.join(directory, f'harmonized_{prefix}_mask.nii.gz')
-            save_nifti(harmMask, mask_final, affine=affine)
+            save_nifti(harmMask, mask_final.astype('uint8'), affine, hdr)
 
 
         ind= int(i/2)
@@ -128,24 +118,25 @@ def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model):
             mapped_cs.append(denoisedImg * shm_coeff[ :,:,:,level])
 
     S_hat= np.dot(np.moveaxis(mapped_cs, 0, -1), B.T)
+    # keep only upper half of the reconstruction signal
+    S_hat= S_hat[..., :int(S_hat.shape[3]/2)]
     np.nan_to_num(S_hat).clip(min= 0., max= 1., out= S_hat)
-
 
     # affine= templateAffine for all Scale_L{i}
     mappedFile= os.path.join(directory, f'{prefix}_mapped_cs.nii.gz')
-    save_nifti(mappedFile, S_hat, affine= affine)
+    save_nifti(mappedFile, S_hat, affine, hdr)
 
     # un-normalize harmonized data
     S_hat_dwi= applymask(S_hat, b0) # overriding applymask function with a nonbinary mask b0
 
     # place b0s in proper indices
-    S_hat_final= stack_b0(bvals, S_hat_dwi, b0)
+    S_hat_final= stack_b0(qb_model.gtab.b0s_mask, S_hat_dwi, b0)
     S_hat_final= applymask(S_hat_final, mask_final)
 
 
     # save harmonized data
     harmImg= os.path.join(directory, f'harmonized_{prefix}.nii.gz')
-    save_nifti(harmImg, S_hat_final, affine= affine)
+    save_nifti(harmImg, S_hat_final, affine, hdr)
 
 
     return (harmImg, harmMask)
@@ -156,6 +147,9 @@ def reconst(imgPath, maskPath, moving, templatePath, preFlag):
     if preFlag:
         imgPath, maskPath = preprocessing(imgPath, maskPath)
 
+    # provide full sampled shm_coeff, qb_model.B
+    # provide imgPath header
+    img = load(imgPath)
     b0, shm_coeff, qb_model = dti_harm(imgPath, maskPath)
 
     directory = os.path.dirname(imgPath)
@@ -169,7 +163,7 @@ def reconst(imgPath, maskPath, moving, templatePath, preFlag):
     antsApply(templatePath, os.path.join(directory, 'harm'), prefix)
 
     print(f'Reconstructing signal from {imgPath} rish features ...')
-    harmImg, harmMask = ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model)
+    harmImg, harmMask = ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model, img.header)
     shutil.copyfile(inPrefix + '.bvec', harmImg.split('.')[0] + '.bvec')
     shutil.copyfile(inPrefix + '.bval', harmImg.split('.')[0] + '.bval')
 
@@ -178,13 +172,14 @@ def reconst(imgPath, maskPath, moving, templatePath, preFlag):
 
     return (imgPath, maskPath, harmImg, harmMask)
 
-def stack_b0(bvals, dwi, b0):
+def stack_b0(b0s_mask, dwi, b0):
 
-    ind= np.where(bvals==0)[0]
+    N= int(len(b0s_mask)/2)
+    ind= np.where(b0s_mask[ :N])[0]
 
     S_hat_final= []
     j= 0
-    for i in range(len(bvals)):
+    for i in range(N):
         if i in ind:
             S_hat_final.append(b0)
         else:
