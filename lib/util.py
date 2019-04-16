2d8f95b6455c5e187e0e4473883e700545d411b3
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-04-16 16:49:42 -0400

    organized import

---
+++ lib/util.py
@@ -0,0 +1,25 @@
+import os, shutil, configparser, sys
+import numpy as np
+from subprocess import check_call
+
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    from nibabel import load, Nifti1Image
+    from dipy.io.image import load_nifti
+    from dipy.io import read_bvals_bvecs
+    from dipy.core.gradients import gradient_table
+    from dipy.reconst.shm import QballModel
+    from dipy.reconst.odf import gfa
+    from dipy.segment.mask import applymask
+
+
+def save_nifti(fname, data, affine, hdr=None):
+    if data.dtype.name=='uint8':
+        hdr.set_data_dtype('uint8')
+    else:
+        hdr.set_data_dtype('float32')
+
+    result_img = Nifti1Image(data, affine, header=hdr)
+    result_img.to_filename(fname)
+
