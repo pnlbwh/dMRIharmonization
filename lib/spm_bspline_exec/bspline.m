9eef8b5c861712eec710814b79086fce45619628
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-07-20 00:00:52 -0400

    add project header

--- lib/spm_bspline_exec/bspline.m
+++ lib/spm_bspline_exec/bspline.m
@@ -1,3 +1,18 @@
+%{
+===============================================================================
+dMRIharmonization (2018) pipeline is written by-
+
+TASHRIF BILLAH
+Brigham and Women's Hospital/Harvard Medical School
+tbillah@bwh.harvard.edu, tashrifbillah@gmail.com
+
+===============================================================================
+See details at https://github.com/pnlbwh/dMRIharmonization
+Submit issues at https://github.com/pnlbwh/dMRIharmonization/issues
+View LICENSE at https://github.com/pnlbwh/dMRIharmonization/blob/master/LICENSE
+===============================================================================
+%}
+
 function []= bspline(inPrefix)
 
     % load lowResImg
