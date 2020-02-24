97ec683d5c104a2fd0bd53216d6ad79d500c22f2
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-02-24 12:00:24 -0500

    omit os.path, couple registration with --force

--- lib/reconstSignal.py
+++ lib/reconstSignal.py
@@ -21,13 +21,14 @@ from preprocess import dti_harm, preprocessing
 from rish import rish
 
 eps= 2.2204e-16
-SCRIPTDIR= os.path.dirname(__file__)
-config = configparser.ConfigParser()
-config.read(f'/tmp/harm_config_{os.getpid()}.ini')
+SCRIPTDIR= dirname(__file__)
+config = ConfigParser()
+config.read(f'/tmp/harm_config_{getpid()}.ini')
 N_shm = int(config['DEFAULT']['N_shm'])
 N_proc = int(config['DEFAULT']['N_proc'])
 debug = int(config['DEFAULT']['debug'])
 n_zero = int(config['DEFAULT']['N_zero'])
+force = int(config['DEFAULT']['force'])
 
 def antsReg(img, mask, mov, outPrefix):
 
@@ -49,14 +50,15 @@ def antsReg(img, mask, mov, outPrefix):
                                '-e', '123456']), shell= True)
         p.wait()
 
+
 def antsApply(templatePath, directory, prefix):
 
     for i in range(0, N_shm+1, 2):
-        moving= os.path.join(templatePath, f'Scale_L{i}.nii.gz')
-        fixed= os.path.join(directory, f'{prefix}_L{i}.nii.gz')
-        output= os.path.join(directory, f'Scale_L{i}_{prefix}.nii.gz')
-        warp= os.path.join(directory, f'ToSubjectSpace_{prefix}1Warp.nii.gz')
-        trans= os.path.join(directory, f'ToSubjectSpace_{prefix}0GenericAffine.mat')
+        moving= pjoin(templatePath, f'Scale_L{i}.nii.gz')
+        fixed= pjoin(directory, f'{prefix}_L{i}.nii.gz')
+        output= pjoin(directory, f'Scale_L{i}_{prefix}.nii.gz')
+        warp= pjoin(directory, f'ToSubjectSpace_{prefix}1Warp.nii.gz')
+        trans= pjoin(directory, f'ToSubjectSpace_{prefix}0GenericAffine.mat')
 
         applyXform(moving, fixed, warp, trans, output)
 
@@ -100,7 +102,7 @@ def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model, hdr):
     for i in range(0, N_shm+1, 2):
 
         # load data and mask
-        fileName= os.path.join(directory, 'harm', f'Scale_L{i}_{prefix}.nii.gz')
+        fileName= pjoin(directory, 'harm', f'Scale_L{i}_{prefix}.nii.gz')
         img= load(fileName).get_data()
 
         if i==0: # compute the skullRingMask from 0th shm
@@ -126,7 +128,7 @@ def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model, hdr):
         if i==0:
             mask_final= findLargestConnectMask(denoisedImg, mask)
             # mask_final= binary_dilation(mask_final, generate_binary_structure(3,1))*1
-            harmMask = os.path.join(directory, f'harmonized_{prefix}_mask.nii.gz')
+            harmMask = pjoin(directory, f'harmonized_{prefix}_mask.nii.gz')
             save_nifti(harmMask, mask_final.astype('uint8'), affine, hdr)
 
 
@@ -141,7 +143,7 @@ def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model, hdr):
     np.nan_to_num(S_hat).clip(min= 0., max= 1., out= S_hat)
 
     # affine= templateAffine for all Scale_L{i}
-    mappedFile= os.path.join(directory, f'{prefix}_mapped_cs.nii.gz')
+    mappedFile= pjoin(directory, f'{prefix}_mapped_cs.nii.gz')
     save_nifti(mappedFile, S_hat, affine, hdr)
 
     # un-normalize harmonized data
@@ -153,37 +155,37 @@ def ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model, hdr):
 
 
     # save harmonized data
-    harmImg= os.path.join(directory, f'harmonized_{prefix}.nii.gz')
+    harmImg= pjoin(directory, f'harmonized_{prefix}.nii.gz')
     save_nifti(harmImg, S_hat_final, affine, hdr)
 
 
     return (harmImg, harmMask)
 
 
-def reconst(imgPath, maskPath, moving, templatePath, preFlag):
+def reconst(imgPath, maskPath, moving, templatePath):
 
-    if preFlag:
-        imgPath, maskPath = preprocessing(imgPath, maskPath)
+    imgPath, maskPath = preprocessing(imgPath, maskPath)
 
     img = load(imgPath)
 
-    directory = os.path.dirname(imgPath)
+    directory = dirname(imgPath)
     inPrefix = imgPath.split('.nii')[0]
-    prefix = os.path.split(inPrefix)[-1] 
-    outPrefix = os.path.join(directory, 'harm', prefix) 
+    prefix = psplit(inPrefix)[-1]
+    outPrefix = pjoin(directory, 'harm', prefix)
     b0, shm_coeff, qb_model = rish(imgPath, maskPath, inPrefix, outPrefix, N_shm)
 
 
     print(f'Registering template FA to {imgPath} space ...')
-    outPrefix = os.path.join(directory, 'harm', 'ToSubjectSpace_' + prefix)
-    fixed = os.path.join(directory, 'dti', f'{prefix}_FA.nii.gz')
-    antsReg(fixed, maskPath, moving, outPrefix)
-    antsApply(templatePath, os.path.join(directory, 'harm'), prefix)
+    outPrefix = pjoin(directory, 'harm', 'ToSubjectSpace_' + prefix)
+    fixed = pjoin(directory, 'dti', f'{prefix}_FA.nii.gz')
+    if force or not isfile(outPrefix+'1Warp.nii.gz'):
+        antsReg(fixed, maskPath, moving, outPrefix)
+    antsApply(templatePath, pjoin(directory, 'harm'), prefix)
 
     print(f'Reconstructing signal from {imgPath} rish features ...')
     harmImg, harmMask = ring_masking(directory, prefix, maskPath, shm_coeff, b0, qb_model, img.header)
-    shutil.copyfile(inPrefix + '.bvec', harmImg.split('.nii')[0] + '.bvec')
-    shutil.copyfile(inPrefix + '.bval', harmImg.split('.nii')[0] + '.bval')
+    copyfile(inPrefix + '.bvec', harmImg.split('.nii')[0] + '.bvec')
+    copyfile(inPrefix + '.bval', harmImg.split('.nii')[0] + '.bval')
 
     return (imgPath, maskPath, harmImg, harmMask)
 
